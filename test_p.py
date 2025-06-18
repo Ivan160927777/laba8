f6b8c125724238b84e9a7ceefbb1c0c342e08fb8
Ivan160927777 <dimasanyavanya2005@gmail.com>
2025-06-18 22:10:55 +0300

    4 tests

--- test_p.py
+++ test_p.py
@@ -1,10 +1,10 @@
 from pattern import *
 
 
-def test_car_status():
+def test_car_status_true():
     car_good = Car("TestModel", True)
     assert car_good._Car__is_good is True
-
+def test_car_status_false():
     car_bad = Car("TestModel", False)
     assert car_bad._Car__is_good is False
 
@@ -15,6 +15,9 @@ def test_subscribe():
 
     car.add_observer(driver)
     assert driver in car.observers
-
+def test_unsubscribe():
+    car = Car("TestModel")
+    driver = Driver("TestDriver")
+    car.add_observer(driver)
     car.remove_observer(driver)
     assert driver not in car.observers
