4896a9d60a06bab6cb940e8bbce20490dde168e3
Ivan160927777 <dimasanyavanya2005@gmail.com>
2025-06-18 19:24:38 +0300

    Программа

--- pattern.py
+++ pattern.py
@@ -1,9 +1,50 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import random
 
-x=1
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('123')
+class Observer:
+    def notify(self,car_model:str, message: str):
+        pass
+
+class Car:
+    __is_good=None
+    def __init__(self, model: str, is_good: bool = None):
+        self.model = model
+        self.__is_good = random.choice([True, False]) if is_good is None else is_good
+        self.observers = []
+
+    def add_observer(self, observer: Observer):
+        self.observers.append(observer)
+
+    def remove_observer(self, observer: Observer):
+        self.observers.remove(observer)
+
+    def __notify_observers(self, message: str):
+        for observer in self.observers:
+            observer.notify(self.model, message)
+
+    def drive(self):
+        if self.__is_good:
+            print(self.model+" едет")
+            if random.random()<0.3:
+                self.__is_good=False
+                self.__notify_observers(" паламалася")
+        else:
+            print(self.model + " не едет")
+
+class Driver(Observer):
+    def __init__(self, name):
+        self.name = name
+
+    def notify(self, model: str, message: str):
+        print(f"Водитель {self.name} получил уведомление: Автомобиль {model} {message}")
+
+if __name__ == "__main__":
+    car = Car("ВАЗ 2107", True)
+    driver1=Driver("Владимир")
+    driver2=Driver("Елена")
+    #car.add_observer(driver1)
+    car.add_observer(driver2)
+    on=""
+    while not on:
+        car.drive()
+        on=input()
--- test_p.py
+++
@@ -1,5 +0,0 @@
-from pattern import x
-def test_1():
-    assert 2==2
-def test_2():
-    assert x==1
